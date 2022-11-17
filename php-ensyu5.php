c810641bc374a7c61d0a73a89d857d9d1fdbf833
raragam <104993317+raragam@users.noreply.github.com>
2022-11-17 04:04:45 +0000

    ensyu5 clear

---
+++ php-ensyu5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+  
+    $name = 'こんにちは！';
+  
+    $name .= 'にんじゃわんこ';
+    // 'こんにちは！'という文字列と$nameを連結してechoしてください
+    
+    echo $name;
+    
+  ?>
+
+</body>
+</html>
