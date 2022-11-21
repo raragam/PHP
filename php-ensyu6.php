e431fcd8253a1932c87142fc1cf5db01ccafced2
raragam <104993317+raragam@users.noreply.github.com>
2022-11-21 06:46:18 +0000

    ensyu6 final

--- php-ensyu6.php
+++ php-ensyu6.php
@@ -1,20 +1,28 @@
-<? php
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
 
-$x = 20;
-if($x > 10){
+  <?php
 
-echo '$xは10より大きい';
+    $x = 99 * 99;
+    $y = 77 * 77;
 
-}
+    // ここにif文を書いていきましょう
+    
+    if($x > 9800){
+      echo '変数xは9800より大きいです';
+    }
+    
+    if($y > 6000){
+      echo '変数yは6000より大きいです';
+    }
+    
+  ?>
 
-?>
-
-<? php
-
-$x = 20;
-
-if($x > 30){
-echo '$xは30より大きい';
-}
-
-?>
+</body>
+</html>
